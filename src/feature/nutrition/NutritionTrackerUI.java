78b561771280c48d35bf7e75f1846111418d73a9
Redowan Ahmed <redowanahmed116@gmail.com>
2025-04-02 03:30:16 +0600

    refactor nutrition tracker UI

--- src/feature/nutrition/NutritionTrackerUI.java
+++ src/feature/nutrition/NutritionTrackerUI.java
@@ -1,83 +1,67 @@
 package feature.nutrition;
 
-import java.io.IOException;
 
+import UI.AbstractFeatureUI;
+
+import java.io.IOException;
+import java.util.*;
+import UI.iCommand;
 import UI.iFeatureUI;
-import feature.healthGoal.InputHandler;
 import utils.JsonDataReader;
 import utils.iDataReader;
 
-public class NutritionTrackerUI implements iFeatureUI{
+public class NutritionTrackerUI extends AbstractFeatureUI{
     private final iNutritionService nutritionService;
-    private final InputHandler inputHandler;
+    private final Map<String, iCommand> commandRegistry;
 
     public NutritionTrackerUI(iNutritionService nutritionService) {
         this.nutritionService = nutritionService;
-        this.inputHandler = new InputHandler();
+        this.commandRegistry = new HashMap<>();
+        registerCommands();
     }
-
+    
+    private void registerCommands() {
+        commandRegistry.put("1", new ViewFoodNutritionCommand(nutritionService, inputProcessor));
+    }
+    
     @Override
     public String getTitle() {
         return "Nutritional Intake Tracker";
     }
-
+    
     @Override
     public void run() {
         boolean exit = false;
         while (!exit) {
             clearScreen();
-            System.out.println("=== Nutritional Intake Tracker ===");
-            System.out.println("1. View Food Nutritional Information");
-            System.out.println("0. Return to Main Menu");
-            String choice = inputHandler.readLine("Enter your choice: ");
-            switch (choice) {
-                case "1":
-                    handleEnterFood();
-                    break;
-                case "0":
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Invalid choice. Try again.");
-                    inputHandler.pause();
-                    break;
+            printMenu();
+            String choice = inputProcessor.readLine("Enter your choice: ");
+            if ("0".equals(choice)) {
+                exit = true;
+            } else {
+                iCommand command = commandRegistry.get(choice);
+                if (command != null) {
+                    command.execute();
+                } else {
+                    inputProcessor.print("Invalid choice. Try again.");
+                    pause();
+                }
             }
         }
     }
-
-    private void handleEnterFood() {
-        String foodName = inputHandler.readLine("Enter the name of the food item: ");
-        FoodItem item = nutritionService.getFoodNutrition(foodName);
-        if (item == null) {
-            System.out.println("Food item not found in the database.");
-        } else {
-            System.out.println("Food item found:");
-            System.out.println("Name: " + item.getName());
-            System.out.println("Calories: " + item.getCalories());
-            System.out.println("Protein: " + item.getProtein());
-            System.out.println("Carbs: " + item.getCarbs());
-            System.out.println("Fats: " + item.getFats());
-            if (item.getVitamins() != null && !item.getVitamins().isEmpty()) {
-                System.out.println("Vitamins: " + item.getVitamins());
-            }
-        }
-        inputHandler.pause();
+    
+    private void printMenu() {
+        inputProcessor.print("=== " + getTitle() + " ===");
+        inputProcessor.print("1. View Food Nutritional Information");
+        inputProcessor.print("0. Return to Main Menu");
     }
 
-    private void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-    }
     public static void main(String[] args) {
         iDataReader dataReader = new JsonDataReader();
-        // Specify the JSON file path for nutritional data.
         String filePath = "nutrition.json";
         try {
-            // Instantiate the JSONNutritionRepository using the JSONDataReader.
             iNutritionRepository nutritionRepository = new JSONNutritionRepository(filePath, dataReader);
-            // Create the service layer for nutrition.
             iNutritionService nutritionService = new NutritionServiceImpl(nutritionRepository);
-            // Instantiate the UI (which implements iFeatureUI).
             iFeatureUI nutritionUI = new NutritionTrackerUI(nutritionService);
             nutritionUI.run();
         } catch (IOException e) {
---
+++ src/feature/nutrition/ViewFoodNutritionCommand.java
@@ -0,0 +1,34 @@
+package feature.nutrition;
+
+import UI.InputProcessor;
+import UI.iCommand;
+
+public class ViewFoodNutritionCommand implements iCommand{
+    private final iNutritionService nutritionService;
+    private final InputProcessor inputProcessor;
+    
+    public ViewFoodNutritionCommand(iNutritionService nutritionService, InputProcessor inputProcessor) {
+        this.nutritionService = nutritionService;
+        this.inputProcessor = inputProcessor;
+    }
+    
+    @Override
+    public void execute() {
+        String foodName = inputProcessor.readLine("Enter the name of the food item: ");
+        FoodItem item = nutritionService.getFoodNutrition(foodName);
+        if (item == null) {
+            inputProcessor.print("Food item not found in the database.");
+        } else {
+            inputProcessor.print("Food item found:");
+            inputProcessor.print("Name: " + item.getName());
+            inputProcessor.print("Calories: " + item.getCalories());
+            inputProcessor.print("Protein: " + item.getProtein());
+            inputProcessor.print("Carbs: " + item.getCarbs());
+            inputProcessor.print("Fats: " + item.getFats());
+            if (item.getVitamins() != null && !item.getVitamins().isEmpty()) {
+                inputProcessor.print("Vitamins: " + item.getVitamins());
+            }
+        }
+        inputProcessor.pause();
+    }
+}
