cff8dcef0921c68154d0e7ddc2062042b26eb712
Redowan Ahmed <redowanahmed116@gmail.com>
2025-04-01 20:54:17 +0600

    modify Json Data reader and writer

--- src/utils/JsonDataReader.java
+++ src/utils/JsonDataReader.java
@@ -1,34 +1,23 @@
 package utils;
 
 import java.io.IOException;
-import java.io.Reader;
 import java.lang.reflect.Type;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
 
 public class JsonDataReader implements iDataReader{
-    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
-
     @Override
-    public <T> T readAll (String filePath, Type typeOfT) throws IOException {
+    public <T> T readAll(String filePath, Type typeOfT) throws IOException {
         Path path = Path.of(filePath);
-
-        if (!Files.exists(path) || Files.size(path) == 0)
-        {
+        if (!Files.exists(path) || Files.size(path) == 0) {
             return null;
         }
-
-        try (Reader reader = Files.newBufferedReader(path, StandardCharsets.UTF_8))
-        {
-            return GSON.fromJson(reader, typeOfT);
-        } catch (JsonSyntaxException e)
-        {
-            throw new IOException("Error parsing json file: " + e.getMessage());
+        String content = Files.readString(path, StandardCharsets.UTF_8);
+        if (typeOfT.equals(String.class)) {
+            return (T) content;
         }
+        throw new UnsupportedOperationException("JSONDataReader supports only reading as String.");
     }
 }
--- src/utils/JsonDataWriter.java
+++ src/utils/JsonDataWriter.java
@@ -3,7 +3,6 @@ package utils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
@@ -11,21 +10,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class JsonDataWriter implements iDataWriter{
-        private static final Gson GSON = new GsonBuilder()
-            .setPrettyPrinting()
-            .create();
     @Override
     public <T> void writeAll(String filePath, T data) throws IOException {
         Path path = Path.of(filePath);
-        
-        if (path.getParent() !=null)
-        {
+        if (path.getParent() != null) {
             Files.createDirectories(path.getParent());
         }
-
-        try (Writer writer = Files.newBufferedWriter(path, StandardCharsets.UTF_8))
-        {
-            GSON.toJson(data, writer);
-        }
+        Files.writeString(path, data.toString(), StandardCharsets.UTF_8);
     }
 }
